529c04353d33506c467dfb8959d373f7f9c93490
Matija Nalis <mnalis-debian@axe.tomsoft.hr>
2018-12-23 22:30:45 +0100

    cleanup on every upgrade

--- postinst.sh
+++ postinst.sh
@@ -3,12 +3,13 @@ set -e
 
 #echo "`date` started $0 with  $@" >> /tmp/tomsoft.debug1
 
+rm -f /var/spool/nagios-local-multi/* 2> /dev/null || true
+rm -f /var/lib/apt/periodic/update-stamp /var/lib/apt/periodic/update-success-stamp || true
+
 if [ "$1" != "configure" ]; then
 	exit 0
 fi
 
-rm -f /var/spool/nagios-local-multi/* 2> /dev/null || true
-rm -f /var/lib/apt/periodic/update-stamp /var/lib/apt/periodic/update-success-stamp || true
 
 chmod 1777 /var/spool/nagios-local-multi
 invoke-rc.d nagios-nrpe-server restart || exit $?
