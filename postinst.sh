c8de1d4c1bc9f378aa38fe8ce484258aaf5d7e48
Matija Nalis <mnalis-debian@axe.tomsoft.hr>
2018-12-23 22:28:46 +0100

    cleanup old timestamps/messages, for they should be regenerated automatically.

--- postinst.sh
+++ postinst.sh
@@ -7,6 +7,9 @@ if [ "$1" != "configure" ]; then
 	exit 0
 fi
 
+rm -f /var/spool/nagios-local-multi/* 2> /dev/null || true
+rm -f /var/lib/apt/periodic/update-stamp /var/lib/apt/periodic/update-success-stamp || true
+
 chmod 1777 /var/spool/nagios-local-multi
 invoke-rc.d nagios-nrpe-server restart || exit $?
 
